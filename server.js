c862b912122b67c206cba55988d142d6b7302dc9
wayne-197 <shashankgautam75@gmail.com>
2021-07-10 08:35:53 +0530

    Added post route for adding books inside an object with coressponding addBook page

--- server.js
+++ server.js
@@ -9,8 +9,14 @@ const port = process.env.PORT || 3000;
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 
+//Parsing incoming post data
+app.use(express.urlencoded());
+app.use(express.json());
 
+//Using for storage of books
+let booksCollection = [];
 
+//Home page route
 app.get("/", (req, res) => {
     res.redirect("/home");
 })
@@ -18,6 +24,24 @@ app.get("/", (req, res) => {
 app.get("/home", (req, res) => {
     res.render("home");
 })
+
+//Adding books route
+app.get("/add-book", (req, res) => {
+    res.render("addBook");
+});
+
+app.post("/add-book", (req, res) => {
+    let book = {
+        name: req.body.book_name,
+        year: req.body.year_publish,
+        author: req.body.author_name
+    }
+    console.log(book);
+    booksCollection.push(book);
+    console.log(booksCollection);
+    res.redirect("/home");
+})
+
 
 server.listen(port, () => {
     console.log("Server started at port " + port);
